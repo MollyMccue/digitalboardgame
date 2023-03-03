d85a0815dce63f085916901ee160984565cbd19c
Briley12 <116219633+Briley12@users.noreply.github.com>
2023-03-03 15:44:08 -0800

    3/3 Tired
    Tired

--- Java.js
+++ Java.js
@@ -1,23 +1,7 @@
-// let board = [
-//     "############    ",
-//     "           #    ",
-//     "           #    ",
-//     "      ###### ###",
+let board = [
+    "############    ",
+    "           #    ",
+    "           #    ",
+    "      ###### ###",
 
-// ]
-
-let board = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]; // all 16 spaces indexed 0 - 15 useful for linear games like monopoly, sorry, etc
-/*
-a 4x4 board
-# # # #
-# # # #
-# # # #
-# # # #
-*/
-//multi-dimensional array
-let gridBoard = [
-    [0,0,0,0], // y 0, x 0-3
-    [0,0,0,0], // y 1, x 0-3
-    [0,0,0,0], // y 2, x 0-3
-    [0,0,0,0]  // y 3, x 0-3
-];
+]
