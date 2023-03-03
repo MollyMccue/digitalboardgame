9bf7aa30a1c18c52ba5d88caa86cea69619b0980
Briley12 <116219633+Briley12@users.noreply.github.com>
2023-03-03 15:32:09 -0800

    3/3 End
    End

--- Java.js
+++ Java.js
@@ -1,92 +1,23 @@
-let board = [
-    "############    ",
-    "           #    ",
-    "           #    ",
-    "      ###### ###",
+// let board = [
+//     "############    ",
+//     "           #    ",
+//     "           #    ",
+//     "      ###### ###",
 
-]
+// ]
 
-// To choose Character with VEX Library
-function CharacterChoose() {
-    vex.dialog.open({
-        message: 'Which character do you want to play?',
-        input: [
-            '<label for="Frog1"><img src="images/Frog1.png" class="imagepadding" /></label>',
-            '<img src="images/Frog1.png" class="imagepadding" />',
-            '<img src="images/Frog2.png" class="imagepadding" />',
-            '<img src="images/Frog3.png" class="imagepadding" />',
-            '<img src="images/Frog4.png" class="imagepadding" />'
-        ].join(''),
-        buttons: [
-            $.extend({}, vex.dialog.buttons.NO, {
-                text: 'Frog4',
-                click: function () {
-                    this.value = 'Frog4';
-                    this.close();
-                }
-            }),
-            $.extend({}, vex.dialog.buttons.NO, {
-                text: 'Frog3',
-                click: function () {
-                    this.value = 'Frog3';
-                    this.close();
-                }
-            }),
-            $.extend({}, vex.dialog.buttons.NO, {
-                text: 'Frog2',
-                click: function () {
-                    this.value = 'Frog2';
-                    this.close();
-                }
-            }),
-            $.extend({}, vex.dialog.buttons.NO, {
-                text: 'Frog1',
-                click: function () {
-                    this.value = 'Frog1';
-                    this.close();
-                }
-            }),
-            $.extend({}, vex.dialog.buttons.NO, {
-                text: 'Frog1',
-                click: function () {
-                    this.value = 'Frog1';
-                    this.close();
-                }
-            })
-        ],
-        callback: function (value) {
-            // Frog1','Frog1','Frog3','Frog2','Frog4
-            switch (value) {
-                case "Frog1":
-                    console.log('You choose Frog1');
-                    break;
-                case "Frog1":
-                    console.log('You choose Frog1');
-                    break;
-                case "Frog3":
-                    console.log('you choose Frog3');
-                    break;
-                case "Frog2":
-                    console.log('You Choose Frog2');
-                    break;
-                case "Frog4":
-                    console.log('You Choose Frog4');
-                    break;
-                default:
-                case "Frog1":
-                    console.log('You choose Frog1');
-                    break;
-            }
-        }
-    })
-}
-
-class Player {
-    constructor(x, y, speed, value) {
-        this.x = x;
-        this.y = y;
-        this.speed = speed;
-        this.sprite = 'images/' + value + '.png';
-
-    }
-}
+let board = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]; // all 16 spaces indexed 0 - 15 useful for linear games like monopoly, sorry, etc
+/*
+a 4x4 board
+# # # #
+# # # #
+# # # #
+# # # #
+*/
+//multi-dimensional array
+let gridBoard = [
+    [0,0,0,0], // y 0, x 0-3
+    [0,0,0,0], // y 1, x 0-3
+    [0,0,0,0], // y 2, x 0-3
+    [0,0,0,0]  // y 3, x 0-3
+];
